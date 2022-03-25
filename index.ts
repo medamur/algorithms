1439e3ef59ff1e86ae083611b194e4fdc78345c5
medamur <1996.popov@gmail.com>
2022-03-25 20:09:27 +0300

    alg hoar

--- index.ts
+++ index.ts
@@ -25,3 +25,33 @@ function findCitiesWay(cities: any[]) {
 }
 
 // console.log(findCitiesWay(cities));
+
+// Алшоритм Хоара
+
+const arrNumebers: number[] =
+  '24524134572524513841093840138193657143657460518734810239847'
+    .split('')
+    .map(Number);
+
+let countHoar = 0;
+
+function sortByHoar(nums: number[]) {
+  countHoar++;
+
+  if (nums.length < 1) return nums;
+
+  const currentNum = nums[0];
+  const less = [];
+  const more = [];
+
+  nums.forEach((elNum, idxNum) => {
+    if (idxNum === 0) return;
+
+    if (elNum > currentNum) more.push(elNum);
+    else less.push(elNum);
+  });
+
+  return [...sortByHoar(less), currentNum, ...sortByHoar(more)];
+}
+
+// console.log(sortByHoar(arrNumebers), 'countHoar: ', countHoar);
