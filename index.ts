579f9ed9f4bbab89c03104436c9bdb195c68e5d6
medamur <1996.popov@gmail.com>
2022-03-25 14:54:39 +0300

    1 increment

--- index.ts
+++ index.ts
@@ -1,7 +1,27 @@
-const arrCities = [
-  { from: 'Москва', to: 'Питер' },
-  { from: 'Питер', to: 'Пермь' },
-  { from: 'Пермь', to: 'Тула' },
-  { from: 'Тула', to: 'Казань' },
+const cities = [
   { from: 'Казань', to: 'Новосибирск' },
+  { from: 'Тула', to: 'Казань' },
+  { from: 'Москва', to: 'Питер' },
+  { from: 'Пермь', to: 'Тула' },
+  { from: 'Питер', to: 'Пермь' },
 ];
+
+function findCitiesWay(cities: any[]) {
+  const startCity = cities.find(
+    (city) => !cities.some((innerCity) => city.from === innerCity.to)
+  );
+
+  const sortedCities = [startCity];
+
+  while (sortedCities.length < cities.length) {
+    const nextCity = cities.find(
+      (city) => city.from === sortedCities[sortedCities.length - 1].to
+    );
+
+    sortedCities.push(nextCity);
+  }
+
+  return sortedCities;
+}
+
+// console.log(findCitiesWay(cities));
