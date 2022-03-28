563a5f43eff7d44797ad3d40a7d34498514cbb0b
medamur <1996.popov@gmail.com>
2022-03-28 18:53:58 +0300

    Graph поиск в ширину

--- index.ts
+++ index.ts
@@ -55,3 +55,39 @@ function sortByHoar(nums: number[]) {
 }
 
 // console.log(sortByHoar(arrNumebers), 'countHoar: ', countHoar);
+
+// Graph поиск в ширину
+
+interface IGraph {
+  [k: string]: string[];
+}
+
+const graph: IGraph = {};
+
+graph.a = ['b', 'c'];
+graph.b = ['f'];
+graph.c = ['d', 'e'];
+graph.d = ['f'];
+graph.e = ['f'];
+graph.f = ['g'];
+
+type typeSearchGraph<T> = (graph: IGraph, start: string, end: string) => T;
+
+const isPath: typeSearchGraph<boolean> = (graph, start, end) => {
+  let queue = [];
+  queue.push(start);
+
+  while (queue.length > 0) {
+    const current = queue.shift();
+
+    if (!graph[current]) graph[current] = [];
+
+    if (graph[current].includes(end)) return true;
+
+    queue = [...queue, ...graph[current]];
+  }
+
+  return false;
+};
+
+console.log(isPath(graph, 'a', 'f'));
